ea7bbf33af040e5eb3637cdbb2e7c9d3348d1e6f
Stephen Coleman <34100773+stcolema@users.noreply.github.com>
2021-12-21 10:33:56 +0000

    Correct error regarding text in package documentation.

---
+++ R/tagmReDraft-package.R
@@ -0,0 +1,15 @@
+#' @title Semi-supervised Multiple Dataset Integration
+#' @description Bayesian integrative clustering/classification. Model estimated
+#' via a Gibbs sampler implemented in C++.
+#' @name tagmReDraft-package
+#' @aliases tagmmdiReDraft tagmReDraft
+#' @docType package
+#' @author Stephen Coleman <stcolema@tcd.ie>, Paul D.W. Kirk, Chris Wallace,
+#' Oliver M. Crook
+#' @keywords package
+#' 
+#' @importFrom ggplot2 aes_string facet_wrap geom_line ggplot label_both
+#' @importFrom stats median rbeta
+#' @importFrom Rcpp evalCpp sourceCpp
+#' @useDynLib tagmReDraft
+NULL
