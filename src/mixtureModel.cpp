271e7ebf7013b9796f8eba2e788e99d8cc752893
Stephen Coleman <34100773+stcolema@users.noreply.github.com>
2022-05-30 12:30:55 +0100

    Delete some old chaff code.

--- src/mixtureModel.cpp
+++ src/mixtureModel.cpp
@@ -99,8 +99,6 @@ void mixtureModel::updateItemAllocation(uword n, vec weights, mat upweigths) {
   comp_prob = ll + log(weights) + log(upweigths.col(n));
   
   // Record the likelihood - this is used to calculate the observed likelihood
-  // likelihood(n) = accu(comp_prob);
-  // observed_likelihood += accu(comp_prob);
   observed_likelihood_vec(n) = accu(comp_prob);
   
   if(fixed(n) == 0) {
@@ -145,46 +143,6 @@ void mixtureModel::updateAllocation(
     updateItemAllocation(n, weights, upweigths);
   }
   );
-  
-  //   if(n >= N) {
-  //     Rcpp::Rcout << "\n\nn: " << n;
-  //   }
-  // for (uword n = 0; n < N; n++) {
-    
-  //   // The mixture-specific log likelihood for each observation in each class
-  //   ll = itemLogLikelihood(X_t.col(n));
-  //   
-  //   
-  //   
-  //   // Update with weights
-  //   comp_prob = ll + log(weights) + log(upweigths.col(n));
-  //   
-  //   // Record the likelihood - this is used to calculate the observed likelihood
-  //   // likelihood(n) = accu(comp_prob);
-  //   observed_likelihood += accu(comp_prob);
-  //   
-  //   
-  //   if(fixed(n) == 0) {
-  //     // Handle overflow problems and then normalise to convert to probabilities
-  //     comp_prob = exp(comp_prob - max(comp_prob));
-  //     comp_prob = comp_prob / sum(comp_prob);
-  //     
-  //     // Save the allocation probabilities
-  //     alloc.row(n) = comp_prob.t();
-  //     
-  //     // Prediction and update
-  //     u = randu<double>( );
-  //     
-  //     labels(n) = sum(u > cumsum(comp_prob));
-  //     outliers(n) = sampleOutlier(ll(labels(n)), outlier_likelihood(n));
-  //   }
-  //   
-  //   // Update the complete likelihood based on the new labelling
-  //   // complete_likelihood += ll(labels(n));
-  //   ll_holder = ll(labels(n));
-  //   
-  // }
-  // );
   
   observed_likelihood = accu(observed_likelihood_vec);
   complete_likelihood = accu(complete_likelihood_vec);
