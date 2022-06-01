d9fb6c901d45abb47a7e5b6458ef9535a2487e95
Stephen Coleman <34100773+stcolema@users.noreply.github.com>
2022-06-01 18:03:26 +0100

    Element access was incorrect.

--- src/categorical.cpp
+++ src/categorical.cpp
@@ -114,7 +114,7 @@ void categorical::sampleKthComponentParameters(
       
       concentration_n = cat_prior_probability(p)(ii) + cat_count;
       
-      category_probabilities(p).(ii, k) = rGamma(1, concentration_n, 1.0).t();
+      category_probabilities(ii, k, p) = rGamma(concentration_n, 1.0);
       // arma::randg(
       //   K, 
       //   arma::distr_param(concentration_n, 1.0)
