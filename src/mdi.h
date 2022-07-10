c338bec76148d7c9f3561972c38b9125a892c19f
Stephen Coleman <stcolema@tcd.ie>
2022-07-10 21:17:08 +0100

    Less informative phi prior.

--- src/mdi.h
+++ src/mdi.h
@@ -49,7 +49,7 @@ public:
     
     // Prior hyperparameters for MDI phi parameters
     phi_shape_prior = 1.0,
-    phi_rate_prior = 0.2,
+    phi_rate_prior = 0.1,
     
     // Model fit
     complete_likelihood = 0.0;
