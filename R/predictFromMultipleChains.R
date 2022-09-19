44585bdc82343f51a87b135f203ee1189393ab67
Stephen Coleman <stcolema@tcd.ie>
2022-09-19 16:28:50 +0200

    Now handling outliers and other quantities previously neglected with examples in documentation

--- R/predictFromMultipleChains.R
+++ R/predictFromMultipleChains.R
@@ -5,10 +5,10 @@
 #' @param burn The number of MCMC samples to drop as part of a burn in.
 #' @param point_estimate_method Summary statistic used to define the point
 #' estimate. Must be ``'mean'`` or ``'median'``. ``'median'`` is the default.
-#' @param construct_psm Logical indicating if PSMs be constructed in the 
-#' unsupervised views. Defaults to FALSE. If TRUE the PSM is constructed and 
+#' @param construct_psm Logical indicating if PSMs be constructed in the
+#' unsupervised views. Defaults to FALSE. If TRUE the PSM is constructed and
 #' this is used to infer the point estimate rather than the sampled partitions.
-#' @param chains_already_processed Logical indicating if the the chains have 
+#' @param chains_already_processed Logical indicating if the the chains have
 #' been processed already.
 #' @returns A named list of quantities related to prediction/clustering:
 #'
@@ -27,39 +27,36 @@
 #'    correspond to items being clustered, rows to MCMC samples.
 #'
 #' @importFrom salso salso
-#' @examples 
+#' @examples
 #' N <- 100
 #' X <- matrix(c(rnorm(N, 0, 1), rnorm(N, 3, 1)), ncol = 2, byrow = TRUE)
 #' Y <- matrix(c(rnorm(N, 0, 1), rnorm(N, 3, 1)), ncol = 2, byrow = TRUE)
-#' 
+#'
 #' truth <- c(rep(1, N / 2), rep(2, N / 2))
 #' data_modelled <- list(X, Y)
-#' 
+#'
 #' V <- length(data_modelled)
-#' 
+#'
 #' # This R is much too low for real applications
 #' R <- 100
 #' thin <- 5
 #' burn <- 10
-#' 
+#'
 #' K_max <- 10
-#' K <- rep(K_max, V) 
+#' K <- rep(K_max, V)
 #' types <- rep("G", V)
-#' 
+#'
 #' n_chains <- 3
 #' mcmc_out <- runMCMCChains(data_modelled, n_chains, R, thin, types, K = K)
 #' predictFromMultipleChains(mcmc_out, burn)
-#' 
+#'
 #' @export
 predictFromMultipleChains <- function(mcmc_outputs,
                                       burn,
                                       point_estimate_method = "median",
                                       construct_psm = FALSE,
                                       chains_already_processed = FALSE) {
-
-  
-  
-  if(chains_already_processed ) {
+  if (chains_already_processed) {
     processed_chains <- mcmc_outputs
   } else {
     # Process the chains, making point estimates and applying a burn-in
@@ -76,17 +73,21 @@ predictFromMultipleChains <- function(mcmc_outputs,
   # Dimensions of the dataset
   V <- first_chain$V
   N <- first_chain$N
+  P <- first_chain$P
   K <- first_chain$K
 
   # Flag for mixture model vs MDI
   multiple_views <- V > 1
-  
+
   # MCMC call
   R <- first_chain$R
   thin <- first_chain$thin
 
   # The type of mixture model used
   types <- first_chain$types
+
+  # The prior on the concentration
+  alpha <- first_chain$alpha
 
   # Indices for views
   view_inds <- seq(1, V)
@@ -116,16 +117,21 @@ predictFromMultipleChains <- function(mcmc_outputs,
   merged_outputs <- list()
   merged_outputs$Semisupervised <- is_semisupervised
   merged_outputs$Overfitted <- is_overfitted
-  
+
+  # I use lists rather than arrays for many of the outputs. This change is not
+  # ideal (should build more consistently), but I prefer lists.
   merged_outputs$allocations <- vector("list", V)
   merged_outputs$allocation_probability <- vector("list", V)
   merged_outputs$prob <- vector("list", V)
   merged_outputs$pred <- vector("list", V)
-  
-  if(construct_psm) {
+  merged_outputs$outliers <- vector("list", V)
+  merged_outputs$N_k <- vector("list", V)
+
+  if (construct_psm) {
     merged_outputs$psm <- vector("list", V)
   }
 
+  # Various parameters of model runs and processing
   merged_outputs$R <- R
   merged_outputs$thin <- thin
   merged_outputs$burn <- burn
@@ -133,18 +139,46 @@ predictFromMultipleChains <- function(mcmc_outputs,
 
   merged_outputs$Point_estimate <- point_estimate_method
 
+  # Key statistics of the data modelled
   merged_outputs$N <- N
+  merged_outputs$P <- P
   merged_outputs$V <- V
   merged_outputs$K <- K
 
+  # Some modelling decisions
+  merged_outputs$types <- types
+  merged_outputs$alpha <- alpha
+
+  # The mass hyper parameter of the component weights and the phi parameters for
+  # MDI
   merged_outputs$phis <- do.call(rbind, lapply(processed_chains, function(x) x$phis))
   merged_outputs$mass <- do.call(rbind, lapply(processed_chains, function(x) x$mass))
-  
+
+  # Some model fit measures (not really working for MDI)
+  merged_outputs$complete_likelihood <- as.matrix(do.call(
+    c,
+    lapply(processed_chains, function(x) x$complete_likelihood)
+  ))
+
+  merged_outputs$evidence <- as.matrix(do.call(
+    c,
+    lapply(processed_chains, function(x) x$evidence)
+  ))
+
+  # The total time for running each chain (this assumes chains are run serially)
+  for (ii in chain_indices) {
+    if (ii == 1) {
+      merged_outputs$Time <- processed_chains[[ii]]$Time
+    } else {
+      merged_outputs$Time <- merged_outputs$Time + processed_chains[[ii]]$Time
+    }
+  }
+
   merged_outputs$weights <- list()
-  for(v in view_inds) {
+  for (v in view_inds) {
     merged_outputs$weights[[v]] <- do.call(rbind, lapply(processed_chains, function(x) x$weights[-dropped_indices, , v, drop = TRUE]))
   }
-  
+
   first_chain <- TRUE
   for (v in view_inds) {
     current_view_is_semi_supervised <- is_semisupervised[v]
@@ -161,10 +195,20 @@ predictFromMultipleChains <- function(mcmc_outputs,
 
       if (in_first_chain) {
         merged_outputs$allocations[[v]] <- .alloc <- .curr_chain$allocations[, , v, drop = TRUE]
+        merged_outputs$outliers[[v]] <- .outliers <- .curr_chain$outliers[, , v, drop = TRUE]
+        merged_outputs$N_k[[v]] <- .n_k <- t(.curr_chain$N_k[, v, , drop = TRUE])
       } else {
-        .prev <- merged_outputs$allocations[[v]]
+        .prev <- .alloc
         .current <- .curr_chain$allocations[, , v, drop = TRUE]
         merged_outputs$allocations[[v]] <- .alloc <- rbind(.prev, .current)
+
+        .out_prev <- .outliers
+        .out_current <- .curr_chain$outliers[, , v, drop = TRUE]
+        merged_outputs$allocations[[v]] <- .outliers <- rbind(.out_prev, .out_current)
+
+        .n_k_prev <- .n_k
+        .n_k_current <- t(.curr_chain$N_k[, v, , drop = TRUE])
+        merged_outputs$N_k[[v]] <- .n_k <- rbind(.n_k_prev, .n_k_current)
       }
 
       if (current_view_is_semi_supervised) {
@@ -174,10 +218,10 @@ predictFromMultipleChains <- function(mcmc_outputs,
         .alloc_prob <- .prev + .curr
       }
     }
-    
-    if(construct_psm) {
+
+    if (construct_psm) {
       merged_outputs$psm[[v]] <- .psm <- createSimilarityMat(.alloc)
-    } 
+    }
 
     if (current_view_is_semi_supervised) {
 
@@ -187,31 +231,30 @@ predictFromMultipleChains <- function(mcmc_outputs,
       merged_outputs$allocation_probability[[v]] <- .alloc_prob
       merged_outputs$prob[[v]] <- .prob <- apply(.alloc_prob, 1, max)
       merged_outputs$pred[[v]] <- apply(.alloc_prob, 1, which.max)
-      
-      if(current_view_is_overfitted) {
+
+      if (current_view_is_overfitted) {
         merged_outputs$pred[[v]] <- suppressWarnings(salso::salso(.alloc))
-      } 
+      }
+    } else {
+      merged_outputs$pred[[v]] <- suppressWarnings(salso::salso(.alloc))
     }
-     else {
-       merged_outputs$pred[[v]] <- suppressWarnings(salso::salso(.alloc))
-     }
   }
-  
+
   # Find the fusion probabilities across views
   # If using a mixture model (i.e. V = 1), this returns a NULL
   merged_outputs$fusion_probabilities <- NULL
-  if(multiple_views) {
+  if (multiple_views) {
     VC2 <- choose(V, 2)
     merged_outputs$fusion_probabilities <- vector("list", VC2)
     entry <- 0
     names <- c()
-    for(v in seq(1, V - 1)) {
-      for(w in seq(v + 1, V)) {
+    for (v in seq(1, V - 1)) {
+      for (w in seq(v + 1, V)) {
         name <- paste0("fused_items_", v, w)
         names <- c(names, name)
         entry <- entry + 1
         merged_outputs$fusion_probabilities[[entry]] <- rep(0, N)
-        for(ii in seq(1, n_chains)) {
+        for (ii in seq(1, n_chains)) {
           merged_outputs$fusion_probabilities[[entry]] <- merged_outputs$fusion_probabilities[[entry]] +
             calcFusionProbabiliy(processed_chains[[ii]], v, w)
         }
@@ -220,6 +263,6 @@ predictFromMultipleChains <- function(mcmc_outputs,
     }
     names(merged_outputs$fusion_probabilities) <- names
   }
-  
+
   merged_outputs
 }
--- R/processMCMCChain.R
+++ R/processMCMCChain.R
@@ -18,6 +18,27 @@
 #'
 #'  * ``pred``: $N$ vector. The predicted class for each sample.
 #'
+#' @examples
+#' N <- 100
+#' X <- matrix(c(rnorm(N, 0, 1), rnorm(N, 3, 1)), ncol = 2, byrow = TRUE)
+#' Y <- matrix(c(rnorm(N, 0, 1), rnorm(N, 3, 1)), ncol = 2, byrow = TRUE)
+#'
+#' truth <- c(rep(1, N / 2), rep(2, N / 2))
+#' data_modelled <- list(X, Y)
+#'
+#' V <- length(data_modelled)
+#'
+#' # This R is much too low for real applications
+#' R <- 100
+#' thin <- 5
+#' burn <- 10
+#'
+#' K_max <- 10
+#' K <- rep(K_max, V)
+#' types <- rep("G", V)
+#'
+#' mcmc_out <- callMDI(data_modelled, R, thin, types, K = K)
+#' processMCMCChain(mcmc_out, burn)
 #' @export
 #' @importFrom stats median
 #' @importFrom salso salso
